23324410fbd98b83c03261b6bc71674dbf13c12c
DevRocker <francesco.danza@yahoo.com>
2014-09-13 16:16:33 +0200

    UI redesign

--- FTPSyncWinConsole/FTPSyncWinConsole/FTPSync.Designer.cs
+++ FTPSyncWinConsole/FTPSyncWinConsole/FTPSync.Designer.cs
@@ -29,32 +29,112 @@
         private void InitializeComponent()
         {
             this.btnSync = new System.Windows.Forms.Button();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.lblHost = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnSync
             // 
-            this.btnSync.Location = new System.Drawing.Point(12, 12);
+            this.btnSync.Location = new System.Drawing.Point(12, 157);
             this.btnSync.Name = "btnSync";
-            this.btnSync.Size = new System.Drawing.Size(188, 164);
+            this.btnSync.Size = new System.Drawing.Size(262, 72);
             this.btnSync.TabIndex = 0;
-            this.btnSync.Text = "Sync";
+            this.btnSync.Text = "Start Sync";
             this.btnSync.UseVisualStyleBackColor = true;
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(72, 12);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(202, 20);
+            this.txtHost.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(72, 38);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(202, 20);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(72, 64);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(202, 20);
+            this.txtPort.TabIndex = 3;
+            // 
+            // lblHost
+            // 
+            this.lblHost.AutoSize = true;
+            this.lblHost.Location = new System.Drawing.Point(9, 19);
+            this.lblHost.Name = "lblHost";
+            this.lblHost.Size = new System.Drawing.Size(29, 13);
+            this.lblHost.TabIndex = 4;
+            this.lblHost.Text = "Host";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(9, 41);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblPassword.TabIndex = 5;
+            this.lblPassword.Text = "Password";
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Location = new System.Drawing.Point(9, 67);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(26, 13);
+            this.lblPort.TabIndex = 6;
+            this.lblPort.Text = "Port";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(12, 90);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(262, 61);
+            this.btnConnect.TabIndex = 8;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
             // 
             // FTPSync
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(214, 191);
+            this.ClientSize = new System.Drawing.Size(284, 241);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.lblPort);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblHost);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtHost);
             this.Controls.Add(this.btnSync);
             this.Name = "FTPSync";
-            this.Text = "Form1";
+            this.Text = "FTPSync";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnSync;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label lblHost;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.Button btnConnect;
     }
 }
 
--- FTPSyncWinConsole/FTPSyncWinConsole/FTPSync.cs
+++ FTPSyncWinConsole/FTPSyncWinConsole/FTPSync.cs
@@ -16,5 +16,7 @@ namespace FTPSyncWinConsole
         {
             InitializeComponent();
         }
+
+
     }
 }
